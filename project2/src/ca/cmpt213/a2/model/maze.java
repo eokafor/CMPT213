7d8638171ca43bece188e2131d6e9fb148d8bcff
Sukhwinder Singh <sukhwind@sfu.ca>
2020-06-15 13:57:34 -0700

    maze class modified.

--- project2/src/ca/cmpt213/a2/model/maze.java
+++ project2/src/ca/cmpt213/a2/model/maze.java
@@ -6,19 +6,35 @@ public class maze {
     int NUM_ROWS;
     int NUM_COLS;
     int grid[][];
-    int x0,y0,x1,y1;
+    int heroX,heroY;
+    int M1X,M1Y;
+    int M2X,M2Y;
+    int M3X,M3Y;
 
 
-    public maze(int NUM_ROWS, int NUM_COLS, int x0, int y0, int x1, int y1) {
+    public maze(int NUM_ROWS, int NUM_COLS) {
         this.NUM_ROWS = NUM_ROWS;
         this.NUM_COLS = NUM_COLS;
-        this.x0 = x0;
-        this.y0 = y0;
-        this.x1 = x1;
-
-
         grid = new int[NUM_ROWS][NUM_COLS];
-        
-
-    }
+        Random objGenerator=new Random();
+        heroX=objGenerator.nextInt(NUM_COLS);
+        heroY=objGenerator.nextInt(NUM_ROWS);
+        do {
+            M1X = objGenerator.nextInt(NUM_COLS);
+        }while(M1X==heroX);
+        do {
+            M1Y = objGenerator.nextInt(NUM_ROWS);
+        }while(M1Y==heroY);
+        do {
+            M2X = objGenerator.nextInt(NUM_COLS);
+        }while(M2X==heroX||M2X==M1X);
+        do {
+            M2Y = objGenerator.nextInt(NUM_ROWS);
+        }while(M2Y==heroY||M2Y==M1Y);
+        do {
+            M3X = objGenerator.nextInt(NUM_COLS);
+        }while(M3X==heroX||M3X==M1X||M3X==M2X);
+        do{
+        M3Y=objGenerator.nextInt(NUM_ROWS);
+        }while(M3Y==heroY||M3Y==M1Y||M3Y==M2Y);
 }
